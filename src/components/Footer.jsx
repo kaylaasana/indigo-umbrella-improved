8a0df349db70f5290c5548976474818b14eb1654
kayla <kayladatte@gmail.com>
2023-08-30 20:22:38 -0700

    added links and logos to footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import github from "../../public/github.png"
+import linkedin from "../../public/linkedin.png"
+import stackoverflow from "../../public/stackoverflow.png"
+
+export default function footer() {
+    return (
+      <footer>
+        <a href="https://github.com/kaylaasana"><img src={github}></img></a>
+        <a href="https://www.linkedin.com/in/kayladatte/"><img src={linkedin}></img></a>
+        <a href="https://stackoverflow.com/users/21856336/kaylaasana"><img src={stackoverflow}></img></a>
+      </footer>
+    );
+  }
+  
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header>
+      <h1> Kayla Datte </h1>
+    </header>
+  );
+}
