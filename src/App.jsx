4c64b67899380a7c1db79481a04225057ab3db1c
kayla <kayladatte@gmail.com>
2023-08-31 15:30:43 -0700

    created main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { Outlet } from 'react-router-dom'
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
 import './App.css'
 
 function App() {
   return (
-    <>
+    <div>
     <Header />
     <Nav />
     <Outlet />
     <Footer />
-    </>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AboutMe from './pages/AboutMe';
+import Project from './pages/Project';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+import ErrorPage from './pages/ErrorPage';
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <AboutMe />
+      },
+      {
+        path: '/project',
+        element: <Project />
+      },
+      {
+        path: '/contact',
+        element: <Contact />
+      },
+      {
+        path: '/resume',
+        element: <Resume />
+      }
+    ]
+  }
+
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <RouterProvider router={router}/>
 )
