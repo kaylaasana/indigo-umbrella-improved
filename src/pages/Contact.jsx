b9305f3912dbe5c476000f90c6c58a7533e5bb50
kayla <kayladatte@gmail.com>
2023-08-31 19:16:23 -0700

    updated error and confirm message handling with contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,23 +1,24 @@
 import { validateEmail } from "../utils/helpers";
+import { useState } from "react";
 
 export default function contactPage() {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
-  const [address, setAddress] = useState("");
   const [message, setMessage] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [confirmMessage, setConfirmMessage] = useState("");
 
   const handleInputChange = (e) => {
     const { target } = e;
     const inputType = target.name;
     const inputValue = target.value;
 
+    setConfirmMessage("");
+
     if (inputType === "email") {
       setEmail(inputValue);
     } else if (inputType === "name") {
       setName(inputValue);
-    } else if (inputType === "address") {
-      setAddress(inputValue);
     } else {
       setMessage(inputValue);
     }
@@ -26,52 +27,80 @@ export default function contactPage() {
   const handleFormSubmit = (e) => {
     e.preventDefault();
 
-    if (!validateEmail(email)) {
-      setErrorMessage("Email is invalid");
+    if (name && email && message) {
+      setConfirmMessage("form submitted");
+      setName("");
+      setEmail("");
+      setMessage("");
+      setErrorMessage("");
       return;
     }
-    setName("");
-    setAddress("");
-    setEmail("");
-    setMessage("");
+    if (email && !validateEmail(email)) {
+      setErrorMessage("email is invalid");
+      return;
+    }
+  };
+
+  const noInput = (e) => {
+    e.preventDefault();
+    if (!name || !email || !message) {
+      setErrorMessage("must complete all fields");
+      return;
+    }
   };
 
   return (
     <div>
       <form className="form" onSubmit={handleFormSubmit}>
-        <input
-          value={email}
-          name="email"
-          onBlur={handleInputChange}
-          type="email"
-          placeholder="email"
-        />
-        <input
-          value={name}
-          name="name"
-          onBlur={handleInputChange}
-          type="text"
-          placeholder="name"
-        />
-        <input
-          value={address}
-          name="address"
-          onBlur={handleInputChange}
-          type="address"
-          placeholder="address"
-        />
-        <input
-          value={message}
-          name="message"
-          onBlur={handleInputChange}
-          type="message"
-          placeholder="message"
-        />
+        <label>
+          Name
+          <br></br>
+          <input
+            value={name}
+            name="name"
+            onChange={handleInputChange}
+            onBlur={noInput}
+            type="text"
+            placeholder="name"
+          />
+        </label>
+        <br></br>
+        <label>
+          E-Mail
+          <br></br>
+          <input
+            value={email}
+            name="email"
+            onChange={handleInputChange}
+            onBlur={noInput}
+            type="email"
+            placeholder="email"
+          />
+        </label>
+        <br></br>
+        <label>
+          Message
+          <br></br>
+          <input
+            value={message}
+            name="message"
+            onChange={handleInputChange}
+            onBlur={noInput}
+            type="message"
+            placeholder="message"
+          />
+        </label>
+        <br></br>
         <button type="submit">Submit</button>
       </form>
       {errorMessage && (
         <div>
           <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
+      {confirmMessage && (
+        <div>
+          <p className="confirm-text">{confirmMessage}</p>
         </div>
       )}
     </div>
