67a2f209177488e1bb6f2820c71f9eafb7a28a46
kayla <kayladatte@gmail.com>
2023-12-03 16:10:53 -0800

    imported component file paths

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { Route, Routes } from 'react-router-dom'
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
+import AboutMe from './pages/AboutMe.jsx';
+import Portfolio from './pages/Portfolio.jsx';
+import Contact from './pages/Contact.jsx';
+import Resume from './pages/Resume.jsx';
+import ErrorPage from './pages/ErrorPage.jsx';
 import './App.css'
 // create App component
 function App() {
