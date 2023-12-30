459e922bbbda502f11675a122ccf51d57d35de9c
kayla <kayladatte@gmail.com>
2023-12-30 13:31:19 -0800

    removed styling in App.css

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function Projects({ projects }) {    
     const [hoverStatus, setHover] = useState(Array(projects.length).fill('false'))
