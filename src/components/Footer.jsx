7e660c35e4f44bf43b433eac0cc9147be82eaf26
kayla <kayladatte@gmail.com>
2023-12-30 16:12:02 -0800

    updated colors and fonts in styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,15 @@ export default function Footer() {
     // return footer element with links to my github, linkedin and stack overflow
     return (
       <footer id="footer" className="d-flex justify-content-center">
-        <a className="p-1" href="https://github.com/kaylaasana"><img src={github} className="img-fluid" width={35}></img></a>
-        <a className="p-1" href="https://www.linkedin.com/in/kayladatte/"><img src={linkedin} className="img-fluid" width={35}></img></a>
-        <a className="p-1" href="https://stackoverflow.com/users/21856336/kaylaasana"><img src={stackoverflow} className="img-fluid" width={35}></img></a>
+        <a className="p-1" href="https://github.com/kaylaasana">
+          <img src={github} className="social img-fluid" width={35}></img>
+        </a>
+        <a className="p-1" href="https://www.linkedin.com/in/kayladatte/">
+          <img src={linkedin} className="social img-fluid" width={35}></img>
+        </a>
+        <a className="p-1" href="https://stackoverflow.com/users/21856336/kaylaasana">
+          <img src={stackoverflow} className="social img-fluid" width={35}></img>
+        </a>
       </footer>
     );
   }
