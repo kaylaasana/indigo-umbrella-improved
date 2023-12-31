e2ba5bada0e10702364d10a4d8b5916942503df5
kayla <kayladatte@gmail.com>
2023-12-31 11:59:24 -0800

    testing background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
 import React from "react";
-import { Outlet } from 'react-router-dom'
-import Header from './components/Header';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 // import AboutMe from './pages/AboutMe.jsx';
 // import Portfolio from './pages/Portfolio.jsx';
 // import Contact from './pages/Contact.jsx';
 // import Resume from './pages/Resume.jsx';
 // import ErrorPage from './pages/ErrorPage.jsx';
-import './App.css'
+import "./App.css";
 import background from "../public/annie-spratt-0ZPSX_mQ3xI-unsplash.jpg";
 // create App component
 function App() {
   return (
-   
-    <div style={{ backgroundImage: `url(${background})` }}>
+    <div
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundRepeat: "no-repeat",
+        width: 500,
+        height: 700
+      }}
+    >
       <Header />
       <Nav />
       <main>
-       <Outlet/>
+        <Outlet />
       </main>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
