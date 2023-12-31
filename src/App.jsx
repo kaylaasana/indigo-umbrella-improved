22ddd099611ff6330028b50331c946986eaa0715
kayla <kayladatte@gmail.com>
2023-12-31 12:06:45 -0800

    testing background image

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,6 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-// import AboutMe from './pages/AboutMe.jsx';
-// import Portfolio from './pages/Portfolio.jsx';
-// import Contact from './pages/Contact.jsx';
-// import Resume from './pages/Resume.jsx';
-// import ErrorPage from './pages/ErrorPage.jsx';
 import "./App.css";
 import background from "../public/annie-spratt-0ZPSX_mQ3xI-unsplash.jpg";
 // create App component
@@ -17,8 +12,6 @@ function App() {
       style={{
         backgroundImage: `url(${background})`,
         backgroundRepeat: "no-repeat",
-        width: 500,
-        height: 700
       }}
     >
       <Header />
