cfb441a9b053af56663cceb40cedb51a157aa841
kayla <kayladatte@gmail.com>
2024-02-08 16:18:12 -0800

    updated alignment on contact me page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -93,7 +93,7 @@ export default function ContactPage() {
           />
         </label>
         <br></br>
-        <button type="submit align-items-center">Submit</button>
+        <button type="submit d-flex align-items-center">Submit</button>
       </form>
       {errorMessage && (
         <div>
